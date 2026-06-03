a7eb251033e6de27acc7b46b388eab69275242d9
sivelio <sivelio75@gmail.com>
2026-06-03 21:36:30 +0300

    fix gmail smtp port

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ admin.initializeApp({
 
 const app = express();
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
+    pass: process.env.MAIL_PASS.replace(/\s/g, "")
   }
 });
   
