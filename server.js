5e44d79aee4c4857ba793b73e781c6a271476ccd
sivelio <sivelio75@gmail.com>
2026-06-04 23:47:21 +0300

    stripe fix

--- server.js
+++ server.js
@@ -106,7 +106,41 @@ app.post("/career-apply", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+app.post("/create-checkout-session", async (req, res) => {
+  console.log("BODY:", req.body);
+  if (!price || !bookingId) {
+  return res.status(400).json({ error: "Missing price or bookingId" });
+}
+  try {
+    const { price, bookingId } = req.body;
 
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "Sivelio Booking",
+            },
+            unit_amount: price,
+          },
+          quantity: 1,
+        },
+      ],
+      success_url: "https://sivelio.com/?success=true",
+      cancel_url: "https://sivelio.com/?canceled=true",
+      metadata: { bookingId },
+    });
+
+    res.json({ url: session.url });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
